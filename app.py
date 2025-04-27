765ad81cfd227235346754bf8dfde7a411a76758
Qaisar Shabbir <qshabbir@paloaltonetworks.com>
2025-04-27 01:41:54 -0700

    Initial commit

--- app.py
+++ app.py
@@ -1,20 +1,11 @@
-from flask import Flask, request, render_template
+# app.py
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def calculate():
-    result = ''
-    if request.method == 'POST':
-        num1 = int(request.form['num1'])
-        num2 = int(request.form['num2'])
-        operation = request.form['operation']
-
-        if operation == 'add':
-            result = num1 + num2
-        elif operation == 'subtract':
-            result = num1 - num2
-    return render_template('index.html', result=result)
+@app.route('/')
+def hello():
+    return "Hello, World!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
--- dockerfile
+++ dockerfile
@@ -5,6 +5,8 @@ WORKDIR /app
 
 COPY . .
 
-RUN pip install -r requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
 
-CMD ["pytest", "test/"]
+EXPOSE 5000
+
+CMD ["python", "app.py"]
